af6f3e98d9289a62c5168f5b8d138c9252a66c95
Matthew Balnis <matthew.balnis@uconn.edu>
2021-02-23 23:53:05 -0500

    Now storing signed in user in state and displaying their name

--- propertypro/src/App.tsx
+++ propertypro/src/App.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
-import { Auth } from "aws-amplify";
+import { Auth, Hub } from "aws-amplify";
 import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib-esm/types"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [user, setUser] = React.useState<any>();
+  const [customAuthState, setCustomAuthState] = React.useState();
+
+  React.useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          setUser(data);
+          break;
+        case "signOut":
+          setUser(undefined);
+          break;
+        case "customOAuthState":
+          setCustomAuthState(data);
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then(user => setUser(user))
+      .catch(() => console.log("Not signed in"));
+  }, [])
+
+  console.log("user");
+  console.log(user);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>Sign in with Google</button>
-        <button onClick={() => Auth.federatedSignIn()}>Sign in</button>
-        <button onClick={() => Auth.signOut()}>Sign out</button>
+        {user ? (
+          <>
+            <p>Hello {user?.signInUserSession?.idToken?.payload?.email}</p>
+            <button onClick={() => Auth.signOut()}>Sign out</button>
+          </>
+        ) : (
+          <>
+            <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>Sign in with Google</button>
+            <button onClick={() => Auth.federatedSignIn()}>Sign in</button>
+          </>
+        )}
       </header>
     </div>
   );
