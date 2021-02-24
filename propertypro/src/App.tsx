37b91c0a8e32154ebdfbc6c4071777010dd3aa0e
Matthew Balnis <matthew.balnis@uconn.edu>
2021-02-23 20:36:46 -0500

    Added authentication resources and configs as well as buttons for signing in

--- propertypro/src/App.tsx
+++ propertypro/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Auth } from "aws-amplify";
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib-esm/types"
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,17 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>Sign in with Google</button>
+        <button onClick={() => Auth.federatedSignIn()}>Sign in</button>
       </header>
     </div>
   );
