98201f1e639bc1f91cb535305d2ca2ab007e6da5
Matthew Balnis <matthew.balnis@uconn.edu>
2021-02-25 23:04:03 -0500

    Minor styling fix

--- propertypro/src/Components/Header/Header.tsx
+++ propertypro/src/Components/Header/Header.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Dropdown from "react-bootstrap/Dropdown";
 import { Link } from "react-router-dom";
 import HeaderDropdown from "./HeaderDropdown";
 
 export default function Header() {
   return (
-    <Navbar bg="secondary" expand="lg">
+    <Navbar className="justify-content-between" bg="secondary" expand="lg">
       <Navbar.Brand as={Link} to="/" className="text-light font-weight-bold">
         Property Pro
       </Navbar.Brand>
