d3801dac7b9d70ff513a003e0cfdfda9de6b43f5
Matthew Balnis <matthew.balnis@uconn.edu>
2021-03-23 21:47:53 -0400

    Added more map controls

--- propertypro/src/Components/Map/Map.tsx
+++ propertypro/src/Components/Map/Map.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 
 export default function Map() {
+  const handleApiLoad = (map: any) => {
+    console.log(map);
+  };
+
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY! }}
       defaultCenter={{ lat: 41.51921, lng: -73.09756 }}
       defaultZoom={10}
+      options={{
+        streetViewControl: true,
+        mapTypeControl: true,
+        fullscreenControl: false,
+      }}
+      yesIWantToUseGoogleMapApiInternals={true}
+      onGoogleApiLoaded={({ map, maps }) => {
+        handleApiLoad(map);
+      }}
     ></GoogleMapReact>
   );
 }
