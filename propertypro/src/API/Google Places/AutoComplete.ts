bb145ae764e20e5c39389314f8808603630f13b7
Matthew Balnis <matthew.balnis@uconn.edu>
2021-04-01 22:39:52 -0400

    Creating the API layer for google places

---
+++ propertypro/src/API/Google Places/AutoComplete.ts
@@ -0,0 +1,55 @@
+import { appendGoogleAPIKeyToUrl, googlePlacesBaseURL, SearchType } from ".";
+import axios from "axios";
+
+export async function googlePlacesAutoComplete(
+  searchText: string,
+  type: SearchType
+) {
+  const googlePlacesSearchType = getGooglePlacesSearchTypeFromSearchType(type);
+  const url = getGooglePlacesAutoCompleteURL(
+    searchText,
+    googlePlacesSearchType
+  );
+
+  const result = await axios.get(url);
+}
+
+function getGooglePlacesAutoCompleteURL(
+  input: string,
+  searchType: GooglePlacesAutoCompleteSearchType
+) {
+  return appendGoogleAPIKeyToUrl(
+    `${googlePlacesAutoCompleteBaseURL}?input=${input}&types=${searchType}`
+  );
+}
+
+const googlePlacesAutoCompleteBaseURL =
+  googlePlacesBaseURL + "/autocomplete/json";
+
+/**
+ * This is a utility function that translates the search type as our app understands it
+ * and returns the resulting Google Places search type.
+ */
+// NOTE: maybe we should have a layer elsewhere where we make these translations, and this file
+// should just be things directly related to the API?
+function getGooglePlacesSearchTypeFromSearchType(
+  searchType: SearchType
+): GooglePlacesAutoCompleteSearchType {
+  switch (searchType) {
+    case SearchType.City:
+      return "(cities)";
+    case SearchType.Address:
+      return "address";
+    default:
+      return undefined;
+  }
+}
+
+/** These are the accepted search types for the Google Places API */
+type GooglePlacesAutoCompleteSearchType =
+  | "geocode"
+  | "address"
+  | "establishment"
+  | "(regions)"
+  | "(cities)"
+  | undefined;
